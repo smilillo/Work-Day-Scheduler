bdb2287200099cb4efef6778237d8612d1d97f6e
smilillo <sofiemilillo@gmail.com>
2023-02-24 17:03:47 -0500

    attempted to create calendar blocks in javascript, needs further work

--- Assets/script.js
+++ Assets/script.js
@@ -2,12 +2,35 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+  // let saveBtnEl = document.getElementsByClassName(".saveBtn");
+  let workHoursArr = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
+  let containerEl = document.getElementsByClassName(".container")
+  
+  // Create Calendar Time Blocks and Columns
+  function createTimeBlocks() {
+    for (i = 0; i < workHoursArr.length; i++) {
+      let row = $("<div>").addClass("row time-block");
+      let timeCol = $("<div>").addClass("hour").text(workHoursArr[i]);
+      let inputCol = $("<textarea>").addClass("description").attr("placeholder", "Type event info here!");
+      let saveBtn = $("<button>").addClass("saveBtn").text("save");
+      $(row).append(timeCol).append(inputCol).append(saveBtn);
+      $(containerEl).append(row);
+    }
+  }
+
+  createTimeBlocks();
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
+
+    // saveBtnEl.addEventListener("click", function() {
+
+    // })
+
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
@@ -23,3 +46,4 @@ $(function () {
   let today = dayjs();
   $("#currentDay").text(today.format("dddd, MMMM D"));
 });
+
