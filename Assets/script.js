2f15e82725737feeda8ac9c87ce033a6ac8c038f
smilillo <sofiemilillo@gmail.com>
2023-02-26 18:54:03 -0500

    push before askbcs help

--- Assets/script.js
+++ Assets/script.js
@@ -3,8 +3,8 @@
 // in the html.
 $(function () {
   // let saveBtnEl = document.getElementsByClassName(".saveBtn");
-  let workHoursArr = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
-  let containerEl = document.getElementsByClassName(".container");
+  // let workHoursArr = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
+  // let containerEl = document.getElementsByClassName(".container");
   
   // Create Calendar Time Blocks and Columns
   // function createTimeBlocks() {
@@ -18,7 +18,7 @@ $(function () {
   //   }
   // }
 
-  createTimeBlocks();
+  // createTimeBlocks();
 
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -37,7 +37,7 @@ $(function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-
+  let currentHour = dayjs().format(H);
 
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
