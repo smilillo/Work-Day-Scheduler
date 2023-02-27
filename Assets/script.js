aaffd34a18eec1a57c3074f8a4c53c57537002dc
smilillo <sofiemilillo@gmail.com>
2023-02-26 21:57:13 -0500

    deleted console log for localStorage

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,7 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
 $(function () {
   // let workHoursArr = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
   // let containerEl = document.getElementsByClassName("timeblockContainer");
@@ -26,7 +27,6 @@ $(function () {
 
       // Save time and description together in local storage
       localStorage.setItem(timeBlock, taskInfo);
-      console.log(localStorage);
     })
 
 // Checks current hour against time-block hour to set background for past, present, or future events
@@ -56,24 +56,10 @@ $(function () {
 
   checkCurrentHour();
 
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
 // Get user input from localStorage and set to time-block descriptions
   $("textarea").each(function() {
     $(this).val(localStorage.getItem($(this).parent().attr("id")))
   });
-  
-  // $("#hour-9 textarea").val(localStorage.getItem("hour-9"));
-  // $("#hour-10 textarea").val(localStorage.getItem("hour-10"));
-  // $("#hour-11 textarea").val(localStorage.getItem("hour-11"));
-  // $("#hour-12 textarea").val(localStorage.getItem("hour-12"));
-  // $("#hour-13 textarea").val(localStorage.getItem("hour-13"));
-  // $("#hour-14 textarea").val(localStorage.getItem("hour-14"));
-  // $("#hour-15 textarea").val(localStorage.getItem("hour-15"));
-  // $("#hour-16 textarea").val(localStorage.getItem("hour-16"));
-  // $("#hour-17 textarea").val(localStorage.getItem("hour-17"));
-
 
 
 // Display the current date in the header of the page.
