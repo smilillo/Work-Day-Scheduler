de83e635fe761af05986847a6b33f89f7d38a071
smilillo <sofiemilillo@gmail.com>
2023-02-26 21:54:23 -0500

    fixed loop, needed parent to traverse up to where id is stored

--- Assets/script.js
+++ Assets/script.js
@@ -60,19 +60,19 @@ $(function () {
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
 // Get user input from localStorage and set to time-block descriptions
-  // $("textarea").each(function() {
-  //   $(this).val(localStorage.getItem($(this).taskInfo))
-  // });
-  // console.log("textarea".val);
-  $("#hour-9 textarea").val(localStorage.getItem("hour-9"));
-  $("#hour-10 textarea").val(localStorage.getItem("hour-10"));
-  $("#hour-11 textarea").val(localStorage.getItem("hour-11"));
-  $("#hour-12 textarea").val(localStorage.getItem("hour-12"));
-  $("#hour-13 textarea").val(localStorage.getItem("hour-13"));
-  $("#hour-14 textarea").val(localStorage.getItem("hour-14"));
-  $("#hour-15 textarea").val(localStorage.getItem("hour-15"));
-  $("#hour-16 textarea").val(localStorage.getItem("hour-16"));
-  $("#hour-17 textarea").val(localStorage.getItem("hour-17"));
+  $("textarea").each(function() {
+    $(this).val(localStorage.getItem($(this).parent().attr("id")))
+  });
+  
+  // $("#hour-9 textarea").val(localStorage.getItem("hour-9"));
+  // $("#hour-10 textarea").val(localStorage.getItem("hour-10"));
+  // $("#hour-11 textarea").val(localStorage.getItem("hour-11"));
+  // $("#hour-12 textarea").val(localStorage.getItem("hour-12"));
+  // $("#hour-13 textarea").val(localStorage.getItem("hour-13"));
+  // $("#hour-14 textarea").val(localStorage.getItem("hour-14"));
+  // $("#hour-15 textarea").val(localStorage.getItem("hour-15"));
+  // $("#hour-16 textarea").val(localStorage.getItem("hour-16"));
+  // $("#hour-17 textarea").val(localStorage.getItem("hour-17"));
 
 
 
